5ebffa68d34296e4261e9c943601bbed0d8986ad
JoerellMorales <20026338+Joerell@users.noreply.github.com>
2025-03-31 19:37:52 +0800

    fourth fix css issue

--- main.py
+++ main.py
@@ -12,3 +12,6 @@ app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "title": "Home"})
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
